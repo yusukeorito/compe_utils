5f343a045bbd3487b6a232324be9b7434370e095
織戸悠輔 <oritoyusuke@oridoyuusukenoMacBook-Air.local>
2022-04-29 22:03:27 +0900

    update version

--- models/model.cat.py
+++ models/model.cat.py
@@ -1,6 +1,9 @@
 #catboost pipeline
 import os
 import gc
+import sys
+from glob import glob
+from pathlib import Path
 import pandas as pd
 import numpy as np
 import matplotlib
@@ -8,6 +11,8 @@ import pickle
 from sklearn.metrics import roc_auc_score
 import catboost as cat
 
+sys.path.append('.')
+from training.validation import get_stratifiedkfold, get_groupkfold
 
 """
 X:pd.DataFrame
@@ -27,13 +32,34 @@ cat_params = {
     'depth': 6,
 }
 
-#atma cup13 ２値分類
-def fit_catboost(X, y, folds, params, categorycal_list=[],add_suffix=''):
-  
-    oof_pred = np.zeros(len(y), dtype=np.float32)#multiclassの時はクラス数に変更
-    models = []
-    fold_unique = sorted(folds.unique())
+#回帰
+cat_params = {
+    'loss_function': 'RMSE',
+    'eval_metric': 'RMSE',
+    'num_boost_round': 10000,
+    'learning_rate': 0.01,
+    'random_state': 42,
+    'task_type': 'CPU',
+    'depth': 6,
+}
 
+
+#atma cup13 ２値分類
+def fit_catboost(X, y, params, folds, categorycal_list=[], add_suffix=''):
+    """
+    cat_params = {
+        'loss_function': 'Logloss',
+        'eval_metric': 'AUC',
+        'num_boost_round': 10000,
+        'learning_rate': 0.03,
+        'random_state': 42,
+        'task_type': 'CPU',
+        'depth': 6,
+    }
+    """
+    oof_pred = np.zeros(len(y), dtype=np.float32)
+
+    fold_unique = sorted(folds.unique())
     for fold in fold_unique:
         idx_train = (folds!=fold)
         idx_valid = (folds==fold)
@@ -50,7 +76,8 @@ def fit_catboost(X, y, folds, params, categorycal_list=[],add_suffix=''):
             label=y_valid,
             cat_features=categorycal_list,
         )
-        model = cat.CatBoostClassifier(**params)
+        model = cat.CatBoostClassifier(**params) #分類
+        # model = cat.CatBoostRegressor(**params) 回帰
         model.fit(
             cat_train,
             early_stopping_rounds=100,
@@ -61,21 +88,45 @@ def fit_catboost(X, y, folds, params, categorycal_list=[],add_suffix=''):
         )
         pickle.dump(model, open(f'cat_fold{fold}{add_suffix}.pkl', 'wb'))
         pred_i = model.predict_proba(x_valid)[:, 1]
+        #pred_i = model.predict(x_valid)[:] 回帰
         oof_pred[x_valid.index] = pred_i
         score = round(roc_auc_score(y_valid, pred_i), 5)
+        #score = round(RMSE(y_valid, pred_i), 5) 回帰
         print(f'Performance of the prediction: {score}\n')
 
-        models.append(model)
-
     score = round(roc_auc_score(y, oof_pred), 5)
+    #score = round(RMSE(y, oof_pred), 5) 回帰
     print(f'All Performance of the prediction: {score}')
-    #del model
+    del model
     gc.collect()
     return oof_pred
 
-def pred_catboost(X, models, add_suffix=''):
-    #models = glob(str(data_dir / f'cat*{add_suffix}.pkl'))
-    #models = [pickle.load(open(model, 'rb')) for model in models]
+def pred_catboost(X, data_dir: Path, categorical_list,  add_suffix=''):
+    models = glob(str(data_dir / f'cat*{add_suffix}.pkl'))
+    models = [pickle.load(open(model, 'rb')) for model in models]
+    X = cat.Pool(X, cat_features=categorical_list)
     preds = np.array([model.predict_proba(X)[:, 1] for model in models])
     preds = np.mean(preds, axis=0)
     return preds
+
+if __name__ == '__main__':
+    train = pd.read_csv('')
+    test = pd.read_csv('')
+    train_X = train.drop(columns=['target'])
+    test_X = test.copy()
+    categorical_list = [] 
+
+
+
+    final_catoof = []
+    final_catsub = []
+    for fold in [5]:
+        folds = get_stratifiedkfold(train, 'target',  fold, 2022)
+        cat_oof = fit_catboost(train_X, train['target'], cat_params, folds, [], f'_cat_numfolds{fold}')
+        cat_sub = pred_catboost = pred_catboost(test_X, Path(''), [], f'_cat_numfolds{fold}')
+        final_catoof.append(cat_oof)
+        final_catsub.append(cat_sub)
+
+    sub = pd.read_csv('')
+    sub['target'] = final_catsub[0]
+    sub.to_csv('submission.csv', index=None)
